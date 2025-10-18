50c024f8fc8e86df89298b38b2ad22ae90caca7d
danmarkowal <danmarkowal@gmail.com>
2025-10-18 17:16:03 +0100

    Update UI of Chrome Extension

--- chrome-extension/src/popup/Popup.svelte
+++ chrome-extension/src/popup/Popup.svelte
@@ -23,115 +23,53 @@
   }
 </script>
 
-<template>
-  <main>
-    <h3>Fact or Fake</h3>
-      <button on:click={factCheck}>
-        Fact check this page
-      </button>
+<img src="src/assets/logo.svg" alt="Logo" />
 
-      {#if content}
-        <div class="result">
-          {@html content}
-        </div>
-      {/if}
-  </main>
-</template>
+<button on:click={factCheck}>
+  Fact check this page
+</button>
+
+{#if content}
+  <div class="result">
+    {@html content}
+  </div>
+{/if}
+
+<a href="about.html" target="_blank">Click here to learn more</a>
 
 <style>
-  :global(:root) {
-    font-family:
-      system-ui,
-      -apple-system,
-      BlinkMacSystemFont,
-      'Segoe UI',
-      Roboto,
-      Oxygen,
-      Ubuntu,
-      Cantarell,
-      'Open Sans',
-      'Helvetica Neue',
-      sans-serif;
+  * {
+    color: #ffffff;
+    font-family: 'Noto Serif', serif;
+  }
 
-    color-scheme: light dark;
+  :global(:root) {
     background-color: #242424;
   }
 
-  @media (prefers-color-scheme: light) {
-    :global(:root) {
-      background-color: #fafafa;
-    }
-
-    a:hover {
-      color: #ff3e00;
-    }
-  }
-
   :global(body) {
-    min-width: 20rem;
-    margin: 0;
-  }
-
-  @media (prefers-color-scheme: light) {
-    :global(:root) {
-      background-color: #fafafa;
-    }
-
-    a:hover {
-      color: #ff3e00;
-    }
-  }
-
-  :global(body) {
-    min-width: 20rem;
-    margin: 0;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  h3 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 1.5rem;
-    font-weight: 200;
-    line-height: 1.2rem;
-    margin: 2rem auto;
-  }
-
-  .calc {
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-    margin: 2rem;
-
-    & > button {
-      font-size: 1rem;
-      padding: 0.5rem 1rem;
-      border: 1px solid #ff3e00;
-      border-radius: 0.25rem;
-      background-color: transparent;
-      color: #ff3e00;
-      cursor: pointer;
-      outline: none;
-
-      width: 3rem;
-      margin: 0 a;
-    }
-
-    & > label {
-      font-size: 1.5rem;
-      margin: 0 1rem;
-    }
+    justify-content: start;
+    min-width: 20rem;
   }
 
   a {
     font-size: 0.5rem;
     margin: 0.5rem;
-    color: #cccccc;
+    color: hsl(0, 0%, 70%);
     text-decoration: none;
+  }
+
+  button {
+    background-color: rgba(255, 255, 255, 0.25);
+    border-radius: 999px;
+    outline-width: 1px;
+    outline-style: solid;
+    outline-color: #ffffff;
+    border-style: none;
+    padding-inline: 1rem;
+    padding-block: 0.5rem;
   }
 </style>
