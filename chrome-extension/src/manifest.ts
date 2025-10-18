a1d030cba512976c855749afc4a49108902923e2
danmarkowal <danmarkowal@gmail.com>
2025-10-18 13:57:10 +0100

    Add fact check button to extension

--- chrome-extension/src/manifest.ts
+++ chrome-extension/src/manifest.ts
@@ -13,35 +13,28 @@ export default defineManifest({
     16: 'img/logo-16.png',
     32: 'img/logo-32.png',
     48: 'img/logo-48.png',
-    128: 'img/logo-128.png',
+    128: 'img/logo-128.png'
   },
   action: {
     default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
-  },
-  options_page: 'options.html',
-  devtools_page: 'devtools.html',
-  background: {
-    service_worker: 'src/background/index.ts',
-    type: 'module',
+    default_icon: 'img/logo-48.png'
   },
   content_scripts: [
     {
       matches: ['http://*/*', 'https://*/*'],
-      js: ['src/contentScript/index.ts'],
-    },
+      js: ['src/contentScript/index.ts']
+    }
   ],
-  side_panel: {
-    default_path: 'sidepanel.html',
-  },
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-32.png', 'img/logo-48.png', 'img/logo-128.png'],
-      matches: [],
-    },
+      resources: [
+        'img/logo-16.png',
+        'img/logo-32.png',
+        'img/logo-48.png',
+        'img/logo-128.png'
+      ],
+      matches: []
+    }
   ],
-  permissions: ['sidePanel', 'storage'],
-  chrome_url_overrides: {
-    newtab: 'newtab.html',
-  },
+  permissions: ['storage', 'scripting', 'activeTab']
 })
