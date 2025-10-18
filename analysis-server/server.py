9cd92cc7d759a20bc783b8fde4e7a2727fb10ee6
Severian0 <darkdalek95@gmail.com>
2025-10-18 17:57:12 +0100

    updated server.py

--- analysis-server/server.py
+++ analysis-server/server.py
@@ -5,16 +5,46 @@ import EarlyAlogirthm
 
 app = fastapi.FastAPI()
 
-@app.post("/factcheck_headlines")
+
+
+@app.get("/")
+def root():
+    return {"message": "You have reached the API."}
+
+
+@app.get("/factcheck_headlines")
 def fact_check_headlines():
+    scraper = webscraper.BBCBusinessScraper()
+    headlines = scraper.fetch_headlines()
+    
+    headline_scores = []
+    for headline in headlines:
+        text = scraper.fetch_article_text(headline)
+        subjectivity, polarity, evidence, total = EarlyAlogirthm.MainScore(text)
+
+        headline_scores.append({
+            "title": headline.title,
+            "link": headline.link,
+            "subjectivity": subjectivity,
+            "polarity": polarity,
+            "evidence": evidence,
+            "total": total
+        })
+    print(headline_scores)
+    # Return automatically as JSON
+    return {"results": headline_scores}
+
+
+
     
     
-    pass
 
 @app.post("/factcheck_article")
 def fact_check_article():
-    return {
 
+    subjectivity,polarity,evidence,total = EarlyAlogirthm.MainScore(text)
+    return {
+        
     }
 
 if __name__ == "__main__":
