91c0ed830e5b65d4d86b3a991caf7dddce0fb335
Severian0 <darkdalek95@gmail.com>
2025-10-18 16:20:21 +0100

    added related articles function to webscraper.py

--- analysis-server/server.py
+++ analysis-server/server.py
@@ -3,9 +3,11 @@ import uvicorn
 
 app = fastapi.FastAPI()
 
-@app.post("/factcheck")
-def fact_check():
-    return "Fact-checking result"
+@app.post("/factcheck_headlines")
+def fact_check_headlines():
+    
+    
+    pass
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
